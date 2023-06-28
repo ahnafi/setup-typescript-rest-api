b59083371b11c99318fb0d0603ab01b8efbdca74
ahnafi <devsulthon@gmail.com>
2023-06-28 12:29:08 +0700

    setup eslint ,pettier,husky

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from 'express'
 
-const app: Application = express();
-const port: Number = 3000;
+const app: Application = express()
+const port: Number = 3000
 
 // routes
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ data: "hello world" });
-});
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: 'hello world' })
+})
 
 app.listen(port, () => {
-  console.log("express listen at port", port);
-});
+  console.log('express listen at port', port)
+})
